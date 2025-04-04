9f64b2bdd12aadc1852e59774e594ffc03d73184
monichre <liamhellis@gmail.com>
2025-04-03 20:58:33 -0500

    Update home page layout and content styling

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,4 +1,5 @@
 import {BoxReveal} from '@/components/magicui/box-reveal'
+import {SignUpButton} from '@clerk/nextjs'
 
 import Image from 'next/image'
 import Link from 'next/link'
@@ -8,7 +9,7 @@ export const Home = () => {
     <main className='flex flex-col justify-center items-center align-middle p-4 w-full'>
       <div className='max-w-lg flex flex-col items-center justify-center align-middle'>
         <Image
-          src='/logo.png'
+          src='/mg-alt.png'
           alt='Restaurant Passport Logo'
           width={300}
           height={300}
@@ -16,26 +17,25 @@ export const Home = () => {
         />
 
         <BoxReveal boxColor={'#E2FFE5'} duration={0.25}>
-          <h2 className='text-3xl font-bold text-center'>
-            Track Your Restaurant Adventures
-          </h2>
+          <p className='text-xl font-bold' style={{color: '#2A643D'}}>
+            Get Started with Your
+          </p>
         </BoxReveal>
 
         <BoxReveal boxColor={'#E2FFE5'} duration={0.35} delay={0.5}>
-          <p className='text-center max-w-lg mt-2'>
-            Keep track of restaurants you've visited, rate your experiences, and
-            create your own{' '}
-            <span className='font-semibold text-[#E2FFE5]'>food passport</span>.
-          </p>
+          <h2
+            className='text-3xl font-bold text-center'
+            style={{color: '#2A643D'}}
+          >
+            Digital Passport
+          </h2>
         </BoxReveal>
 
         <BoxReveal boxColor={'#E2FFE5'} duration={0.5} delay={0.8}>
           <div className='mt-6'>
-            <p>
-              Discover new culinary experiences in your area <br />
-              Earn rewards and special offers at participating restaurants{' '}
-              <br />
-              Share your favorite spots with friends and fellow foodies
+            <p className='text-center max-w-lg mt-2'>
+              Your foodie journey just went digital. Sign in, check in, Scan &
+              win big!
             </p>
           </div>
         </BoxReveal>
@@ -44,18 +44,21 @@ export const Home = () => {
           <div className='flex gap-4 items-center flex-row mt-8 justify-center align-middle w-full'>
             <div className='flex gap-4 justify-center align-middle items-center w-min mx-auto'>
               <Link
-                href='/sign-up'
-                className='rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#E2FFE5] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
+                href='/deals'
+                style={{background: '#208F54'}}
+                className='bg=[#208F54] border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#E2FFE5] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer'
               >
-                Get Started
+                See Deals
               </Link>
 
-              <Link
-                href='/deals'
-                className='rounded-full bg-[#e0d9d1] relative z-40 border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#E2FFE5] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
+              <div
+                style={{
+                  background: '#43E790',
+                }}
+                className='bg-[#43E790] relative z-40 border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#E2FFE5] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 text-black cursor-pointer'
               >
-                Browse Deals
-              </Link>
+                <SignUpButton>Get Started</SignUpButton>
+              </div>
             </div>
           </div>
         </BoxReveal>
